c71bfea5ff2a3fc1495074d238890ef5dddf4294
sanjaysabnani <sabnani.sanjay@ymail.com>
2020-04-25 11:39:08 +0300

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Sanjay Sabnani on 4/25/20.
+//  Copyright © 2020 Sanjay Sabnani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
